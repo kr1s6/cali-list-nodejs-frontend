3fef86a6237753a8737a86b2fab51733d7012344
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2025-04-30 23:13:06 +0200

    navbar with addidtional buttons

---
+++ src/components/navbar.js
@@ -0,0 +1,38 @@
+import { Suspense } from "react";
+import ThemeChangeBtn from "./theme-change";
+
+export default function Navbar() {
+    return (
+        <div className="navbar bg-base-100 shadow-md justify-around">
+            <div className="navbar p-0 min-h-0 max-w-5xl mx-10">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            <li><a>Item 1</a></li>
+                            <li><a>Parent</a></li>
+                            <li><a>Item 3</a></li>
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">CaliList</a>
+                    <ThemeChangeBtn></ThemeChangeBtn>
+
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a>Search</a></li>
+                        <li><a>Social</a></li>
+                        <li><a>Forum</a></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn btn-ghost">Login</a>
+                    <a className="btn btn-primary">Sign Up</a>
+                </div>
+            </div>
+        </div>
+    );
+}
